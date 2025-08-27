983b83ecad621d61d9ac7702186f7be9691fb68e
Gagema <gabrielgmazolli@gmail.com>
2025-08-27 19:39:20 -0300

    inserção de threads e fila

--- server.py
+++ server.py
@@ -2,9 +2,51 @@
 import socket
 import threading
 import time
+import queue
 
 HOST = '127.0.0.1'
 PORT = 55555
+q= queue.Queue()
+def thread2():
+    lasttime=time.time()
+    lasttime2=time.time()
+    while True:
+        if time.time()-lasttime >60:
+            timenow = time.localtime()
+            tempo = f"{timenow.tm_hour}:{timenow.tm_min}:{timenow.tm_sec}"
+            conn.sendall(tempo.encode('utf-8'))
+            print(tempo)
+            lasttime=time.time()
+        if time.time() - lasttime2 > 5:
+            mensagem2=q.get()
+            resposta = f"{mensagem2}"
+            print(resposta)
+            conn.sendall(mensagem2.encode('utf-8'))
+
+
+            lasttime2 = time.time()
+
+
+
+def thread1():
+    while True:
+
+        # recebe dados do cliente, 1024 é o tamanho máx do pacote
+        dados = conn.recv(1024)
+
+        # se receber dados vazios, o cliente encerrou a conexão
+        if not dados:
+            break
+
+        # decodifica e exibe a mensagem
+        mensagem = dados.decode('utf-8')
+        print(f"{apelido}: {mensagem}")
+        q.put(mensagem)
+
+        # envia uma resposta genérica de volta
+    print("Conexão encerrada.")
+
+
 
 # cria um obj socket, o socket.AF_INET significa que estamos usando endereços IPv4 e o socket.SOCK_STREAM significa que estamos usando TCP e o with garante que o socket será fechado corretamente depois do bloco de código
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -27,22 +69,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
         apelido = conn.recv(1024).decode('utf-8')
         print(f"Novo cliente conectado: {apelido}")
+        thread_recepcao = threading.Thread(target=thread2)
+        thread_recepcao.start()
+        thread_insercao = threading.Thread(target=thread1)
+        thread_insercao.start()
+        thread_recepcao.join()
+        thread_insercao.join()
 
         # loop infinito para o server ainda receber mensagens
-        while True:
-            # recebe dados do cliente, 1024 é o tamanho máx do pacote
-            dados = conn.recv(1024)
 
-            # se receber dados vazios, o cliente encerrou a conexão
-            if not dados:
-                break
 
-            # decodifica e exibe a mensagem
-            mensagem = dados.decode('utf-8')
-            print(f"{apelido}: {mensagem}")
-
-            # envia uma resposta genérica de volta
-            resposta = f"{mensagem}"
-            conn.sendall(resposta.encode('utf-8'))
-
-    print("Conexão encerrada.")
