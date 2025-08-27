333b9b021b997f41db880da9b5390ca69a8734fb
Gagema <gabrielgmazolli@gmail.com>
2025-08-27 17:31:27 -0300

    adicionado o sistema de horários

--- client.py
+++ client.py
@@ -1,35 +1,36 @@
-#client.py
+# client.py
 import socket
 
-
 HOST = '127.0.0.1'
-PORT = 65432        
+PORT = 55555
 
-#cria um obj socket, o socket.AF_INET significa que estamos usando endereços IPv4 e o socket.SOCK_STREAM significa que estamos usando TCP e o with garante que o socket será fechado corretamente depois do bloco de código
+# cria um obj socket, o socket.AF_INET significa que estamos usando endereços IPv4 e o socket.SOCK_STREAM significa que estamos usando TCP e o with garante que o socket será fechado corretamente depois do bloco de código
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     try:
-        #tenta conectar ao servidor, se o servidor não stiver rodando, essa linha falha
+        # tenta conectar ao servidor, se o servidor não stiver rodando, essa linha falha
         s.connect((HOST, PORT))
         print("Conectado ao servidor. Digite suas mensagens. (Digite 'sair' para fechar a conexão)")
+        tempo=s.recv(1024)
+        print(f" {tempo.decode('utf-8')}")
 
         apelido = input("Digite seu apelido: ")
         s.sendall(apelido.encode('utf-8'))
-        
-        #loop infinito para o cliente ainda enviar mensagens
+
+        # loop infinito para o cliente ainda enviar mensagens
         while True:
-            #pede para o usuário digitar a mensagem
+            # pede para o usuário digitar a mensagem
             mensagem = input("> ")
 
-            #se a mensagem for 'sair', o cliente encerra a conexão
+            # se a mensagem for 'sair', o cliente encerra a conexão
             if mensagem.lower() == 'sair':
                 break
 
-            #envia a mensagem codificada em bytes
+            # envia a mensagem codificada em bytes
             s.sendall(mensagem.encode('utf-8'))
 
-            #aguarda e recebe a resposta do servidor
+            # aguarda e recebe a resposta do servidor
             dados = s.recv(1024)
-            print(f"Você digitou: {dados.decode('utf-8')}")#exibe a resposta do servidor
+            print(f"Você digitou: {dados.decode('utf-8')}")  # exibe a resposta do servidor
 
     except ConnectionRefusedError:
         print("Não foi possível conectar. Verifique se o servidor está em execução.")
--- server.py
+++ server.py
@@ -1,46 +1,48 @@
-#server.py
+# server.py
 import socket
 import threading
 import time
 
 HOST = '127.0.0.1'
-PORT = 65432        
+PORT = 55555
 
-#cria um obj socket, o socket.AF_INET significa que estamos usando endereços IPv4 e o socket.SOCK_STREAM significa que estamos usando TCP e o with garante que o socket será fechado corretamente depois do bloco de código
+# cria um obj socket, o socket.AF_INET significa que estamos usando endereços IPv4 e o socket.SOCK_STREAM significa que estamos usando TCP e o with garante que o socket será fechado corretamente depois do bloco de código
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))#vincula o socket ao endereço e porta definidos
-    s.listen()#coloca o socket em modo de escuta
+    s.bind((HOST, PORT))  # vincula o socket ao endereço e porta definidos
+    s.listen()  # coloca o socket em modo de escuta
     print(f"Servidor escutando em {HOST}:{PORT}")
-    timenow=time.time()
-    printf(f"{timenow}")
-    
-    conn, addr = s.accept()#aceita uma conexão de um cliente
-    #conn é o novo socket que será usado para a comunicação com o cliente, e addr é uma tupla com o endereço do cliente que se conectou
-    
-    #usa o novo socket para comunicação
+
+
+    conn, addr = s.accept()  # aceita uma conexão de um cliente
+    # conn é o novo socket que será usado para a comunicação com o cliente, e addr é uma tupla com o endereço do cliente que se conectou
+
+    # usa o novo socket para comunicação
 
     with conn:
         print(f"Conectado por {addr}")
-        
-        
+        timenow = time.localtime()
+        tempo= f"{timenow.tm_hour}:{timenow.tm_min}:{timenow.tm_sec}:CONECTADO!!"
+        conn.sendall(tempo.encode('utf-8'))
+        print(tempo)
+
         apelido = conn.recv(1024).decode('utf-8')
         print(f"Novo cliente conectado: {apelido}")
 
-        #loop infinito para o server ainda receber mensagens
+        # loop infinito para o server ainda receber mensagens
         while True:
-            #recebe dados do cliente, 1024 é o tamanho máx do pacote
+            # recebe dados do cliente, 1024 é o tamanho máx do pacote
             dados = conn.recv(1024)
 
-            #se receber dados vazios, o cliente encerrou a conexão
+            # se receber dados vazios, o cliente encerrou a conexão
             if not dados:
                 break
-            
-            #decodifica e exibe a mensagem
+
+            # decodifica e exibe a mensagem
             mensagem = dados.decode('utf-8')
             print(f"{apelido}: {mensagem}")
 
-            #envia uma resposta genérica de volta
+            # envia uma resposta genérica de volta
             resposta = f"{mensagem}"
             conn.sendall(resposta.encode('utf-8'))
-            
+
     print("Conexão encerrada.")
